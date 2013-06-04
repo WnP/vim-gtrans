9b2321e2afddc52242e49ee9b699de59e4e933ce
Steeve <steevechailloux@gmail.com>
2013-06-04 10:04:35 +0200

    first step refactoring

--- plugin/gtrans.py
+++ plugin/gtrans.py
@@ -3,49 +3,72 @@ import translate
 import urllib2
 import string
 
-to_lang = vim.eval('g:gtrans_output_language') or 'fr'
-from_lang = vim.eval('g:gtrans_input_language') or 'en'
-trans = translate.Translator(to_lang=to_lang, from_lang=from_lang)
 
-encoding = vim.eval('&encoding') or 'utf-8'
-unwordable = ''.join((string.punctuation, string.whitespace))
+def __do_visual(buff, cur, res=''):
+    cursor = cur.window.cursor
 
-cur = vim.current
-markL = cur.buffer.mark('<')
-markR = cur.buffer.mark('>')
-cursor = cur.window.cursor
+    if cursor[0] == buff.mark('<')[0]:
 
-res = ''
+        if buff.mark('<')[0] == buff.mark('>')[0]:
 
-if markL and markR and cursor[1] == 0 and markL[0] <= cursor[0] <= markR[0]:
-    # visual mode
-    if cursor[0] == markL[0]:
-        if markL[0] == markR[0]:
-            res = cur.buffer[markL[0] - 1][markL[1]:markR[1]]
+            return buff[
+                buff.mark('<')[0] - 1][
+                    buff.mark('<')[1]:buff.mark('>')[1]]
         else:
-            res = cur.buffer[markL[0] - 1][markL[1]:]
-            for x in xrange(markR[0] - markL[0] - 1):
-                res = ' '.join((res, cur.buffer[markL[0] + x]))
-            res = ' '.join((res, cur.buffer[markR[0] - 1][:markR[1]]))
-else:
-    # cursor mode
-    line = vim.current.buffer[cursor[0] - 1]
+            res = buff[buff.mark('<')[0] - 1][buff.mark('<')[1]:]
+
+            for x in xrange(buff.mark('>')[0] - buff.mark('<')[0] - 1):
+                res = ' '.join((res, buff[buff.mark('<')[0] + x]))
+
+            return ' '.join((
+                res,
+                buff[buff.mark('>')[0] - 1][:buff.mark('>')[1]]))
+
+
+def __do_cursor(cur, res=''):
+    cursor = cur.window.cursor
+    line = cur.buffer[cursor[0] - 1]
+    unwordable = ''.join((string.punctuation, string.whitespace))
+
     x = line[cursor[1]]
     i = 0
     while x not in unwordable and i + cursor[1] >= 0:
         res = ''.join((x, res))
         i -= 1
         x = line[cursor[1] + i]
+
     x = line[cursor[1] + 1]
     i = 1
     while x not in unwordable and i + cursor[1] <= len(line):
         res = "".join((res, x))
         i += 1
         x = line[cursor[1] + i]
+    return res
 
-translated = ''
-try:
-    translated = trans.translate(res)
-except urllib2.HTTPError:
-    pass
-print translated.encode(encoding)
+
+def vim_gtranslate():
+    to_lang = vim.eval('g:gtrans_output_language') or 'fr'
+    from_lang = vim.eval('g:gtrans_input_language') or 'en'
+    trans = translate.Translator(to_lang=to_lang, from_lang=from_lang)
+
+    encoding = vim.eval('&encoding') or 'utf-8'
+
+    buff = vim.current.buffer
+    cur = vim.current
+    cursor = cur.window.cursor
+    res = ''
+
+    if buff.mark('<') and buff.mark('>') and cursor[1] == 0 \
+       and buff.mark('<')[0] <= cursor[0] <= buff.mark('>')[0]:
+        # visual mode
+        res = __do_visual(buff, cur)
+    else:
+        # cursor mode
+        res = __do_cursor(cur)
+
+    translated = ''
+    try:
+        translated = trans.translate(res)
+    except urllib2.HTTPError:
+        pass
+    print translated.encode(encoding)
--- plugin/gtrans.vim
+++ plugin/gtrans.vim
@@ -2,10 +2,12 @@
 let g:gtrans_input_language = 'en'
 let g:gtrans_output_language = 'fr'
 
-let s:gtrans_path_sdir = expand('<sfile>:p:h')
+""let s:gtrans_path_sdir = expand('<sfile>:p:h')
+pyfile <sfile>:p:h/gtrans.py
 
-function! Gtrans()
-execute 'pyfile '.s:gtrans_path_sdir.'/gtrans.py'
+function! Vim_gtranslate()
+""execute 'pyfile '.s:gtrans_path_sdir.'/gtrans.py'
+python vim_gtranslate()
 endfunction
 
-command! -nargs=0 Gtrans call Gtrans()
+command! -nargs=0 Gtrans call Vim_gtranslate()
